21c7151bde3e7141724440304d19afa79b98fdd8
roxanap33 <roxana.ciocian@principal33.com>
2024-03-07 16:50:12 +0200

    111: Complete all exercises using Vanilla CSS

--- 01-starting-project/src/components/Header.jsx
+++ 01-starting-project/src/components/Header.jsx
@@ -1,11 +1,17 @@
-import logo from '../assets/logo.png';
+import logo from "../assets/logo.png";
+import classes from "./Header.module.css";
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p
+        className={classes.paragraph}
+        style={{ textAlign: "center", color: "#a39191", margin: 0 }}
+      >
+        A community of artists and art-lovers.
+      </p>
     </header>
   );
 }
