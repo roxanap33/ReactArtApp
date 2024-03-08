7c3d5d7b1ed88fbffd78232a92e72cd461a741ba
roxanap33 <roxana.ciocian@principal33.com>
2024-03-08 15:20:04 +0200

    116: Complete all exercises using styled components

--- 01-starting-project/src/components/AuthInputs.jsx
+++ 01-starting-project/src/components/AuthInputs.jsx
@@ -1,4 +1,14 @@
 import { useState } from "react";
+import styled from "styled-components";
+import { Button } from "./Button";
+import CustomInput from "./Input";
+
+const ControlContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1.5rem;
+`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
@@ -22,40 +32,37 @@ export default function AuthInputs() {
 
   return (
     <div id="auth-inputs">
-      <div className="controls">
+      <ControlContainer>
         <p className="paragraph">
-          <label className={`label ${emailNotValid ? "invalid" : ""}`}>
-            Email
-          </label>
-          <input
+          <CustomInput
+            label="E-mail"
+            $invalid={emailNotValid} //dollar sign to show that this prop is used only for styling
             type="email"
             // style={{
             //   backgroundColor: emailNotValid ? "#f73f3f" : "#d1d5db",
             // }}
-            className={emailNotValid ? "invalid" : undefined}
             onChange={(event) => handleInputChange("email", event.target.value)}
           />
         </p>
         <p>
-          <label className={`label ${passwordNotValid ? "invalid" : ""}`}>
-            Password
-          </label>
-          <input
+          <CustomInput
+            label="Password"
+            $invalid={passwordNotValid}
             type="password"
-            className={passwordNotValid ? "invalid" : undefined}
+            //className={passwordNotValid ? "invalid" : undefined}
             onChange={(event) =>
               handleInputChange("password", event.target.value)
             }
           />
         </p>
-      </div>
+      </ControlContainer>
       <div className="actions">
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className="button" onClick={handleLogin}>
+        <Button className="button" onClick={handleLogin}>
           Sign In
-        </button>
+        </Button>
       </div>
     </div>
   );
